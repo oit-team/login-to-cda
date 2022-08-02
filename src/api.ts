55c858e2ab5e11179f16830b14386649a75b96f8
邱旭 <inlinger@outlook.com>
2022-08-02 11:32:50 +0800

    init

---
+++ src/api.ts
@@ -0,0 +1,49 @@
+interface ResponseData<T> {
+  code: number
+  msg: string
+  result: T
+}
+
+export function request<T = any>(url: string, data?: any, method: 'POST' | 'GET' = 'POST') {
+  return fetch(url, {
+    method,
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then(async res => {
+    if (res.ok) {
+      const json = await res.json() as ResponseData<T>
+      if (json.code === 0) return json.result
+    }
+    return Promise.reject(res)
+  })
+}
+
+export function get<T>(url: string, data?: any) {
+  return request<T>(url, data, 'GET')
+}
+
+export function post<T>(url: string, data?: any) {
+  return request<T>(url, data, 'POST')
+}
+
+export function getKey() {
+  return get<{
+    publicKey: string
+  }>('/cda/sys/v2/secretKey/getKey')
+}
+
+export function login(params: {
+  username: string
+  password: string
+}) {
+  return post<{
+    accesstoken: string
+  }>('/cda/oauth/v2/login', {
+    code: '',
+    type: 'sys',
+    uuid: '',
+    ...params,
+  })
+}
---
+++ src/main.ts
@@ -0,0 +1,50 @@
+import { sm2 } from 'sm-crypto'
+import { getKey, login } from './api'
+
+function assert(value: any, msg?: string): asserts value {
+  if (!value) throw new TypeError(msg)
+}
+
+async function setup() {
+  const searchParams = new URLSearchParams(location.href.split('?')[1])
+  const base64Data = searchParams.get('data')
+
+  assert(base64Data, 'data is null')
+
+  const params = atob(base64Data).split('XX@XX')
+
+  console.log('参数 =>', params)
+
+  const username = params[2]
+  const password = params[3]
+  const url = params[4]
+
+  assert(username, 'username is null')
+  assert(password, 'password is null')
+  assert(url, 'url is null')
+
+  const { publicKey: publicKeyRaw } = await getKey()
+  const publicKey = publicKeyRaw.split('\r\n')[0]
+  const encrypted = sm2.doEncrypt(password, publicKey, 0)
+
+  console.log('publicKey =>', publicKey)
+  console.log('encrypted =>', encrypted)
+
+  const { accesstoken } = await login({
+    username,
+    password: `04${encrypted}`,
+  })
+
+  console.log('accesstoken =>', accesstoken)
+
+  localStorage.setItem('cda__ACCESS_TOKEN', accesstoken)
+  localStorage.setItem('cda__CODE', publicKey)
+
+  location.replace(url)
+}
+
+setup()
+  .catch(e => {
+    console.error(e)
+    window.alert(e)
+  })
